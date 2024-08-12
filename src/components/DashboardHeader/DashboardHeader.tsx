a9ad1dbcd678d8de7602402286c1a0d25b43165d
bradewalder <brade.walder@outlook.com>
2024-08-12 18:09:39 +1000

    Dashboard UI Updates

--- src/components/DashboardHeader/DashboardHeader.tsx
+++ src/components/DashboardHeader/DashboardHeader.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch } from 'react-icons/bs';
 import styles from '../../routes/Dashboard/Dashboard.module.css';
 
 const DashboardHeader: React.FC = () => {
 	return (
 		<header className={styles.header}>
-			<div className={styles.headerLeft}>
-				<input type="text" className={styles.searchInput} placeholder="Search..." />
-				<BsSearch className={styles.icon} />
-			</div>
-			<div className={styles.headerRight}>
-				<BsFillBellFill className={styles.icon} />
-				<BsFillEnvelopeFill className={styles.icon} />
-				<BsPersonCircle className={styles.icon} />
-			</div>
+			
 		</header>
 	);
 };
